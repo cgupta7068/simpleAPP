5dd2c9d9e3b796ddf2b0197d1fd02ebf431d0d07
Chandan Gupta <cgupta7068@gmail.com>
2026-03-26 21:27:26 +0530

    remove the unnecessary logg

--- lendef/src/main.jsx
+++ lendef/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId="216690250177-nai4sul9b700ivcuraa3uv6v0ogj09f2.apps.googleusercontent.com">
